2c7d38472453b75d0d53653adee5cf096eb62b0c
Serega <aleksei87-2010@mail.ru>
2024-10-19 14:55:43 +0300

    changed paths

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+        base: "/Eldorado/",
     server: {
         open: '/index.html', 
-        base: "/Eldorado/"
     }
 });
